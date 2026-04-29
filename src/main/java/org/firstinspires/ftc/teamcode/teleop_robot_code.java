e289dc2e9a7e20e25f441bc7d50099853ac9eb00
rteam <RTeam@pymblelc.nsw.edu.au>
2026-04-29 18:29:26 +1000

    robot_code

---
+++ src/main/java/org/firstinspires/ftc/teamcode/teleop_robot_code.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/*
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="a4_robot_code", group="1")
+
+public class teleop_robot_code extends OpMode
+{
+    // Declare OpMode members.
+    private DcMotor FR, FL, BR, BL, I, PF, SL, SR;
+
+    private CRServo F;
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        FR = hardwareMap.get(DcMotor.class,"FR");
+        FL = hardwareMap.get(DcMotor.class,"FL");
+        BR= hardwareMap.get(DcMotor.class,"BR");
+        BL = hardwareMap.get(DcMotor.class,"BL");
+        I = hardwareMap.get(DcMotor.class,"I");
+        PF = hardwareMap.get(DcMotor.class,"PF");
+        SL = hardwareMap.get(DcMotor.class,"SL");
+        SR = hardwareMap.get(DcMotor.class,"SR");
+
+        F = hardwareMap.get(CRServo.class,"F");
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+
+        if (gamepad1.a) {
+            // execution
+            I.setPower(-0.5);
+        } else {
+            I.setPower(0);
+        }
+
+        if (gamepad1.b) {
+            // execution
+            PF.setPower(-0.5);
+        } else {
+            PF.setPower(0);
+        }
+
+        if (gamepad1.x) {
+            // execution
+            F.setPower(0.5);
+        } else {
+            F.setPower(0);
+        }
+
+        if (gamepad1.y) {
+            // execution
+            SR.setPower(0.5);
+            SL.setPower(0.5);
+        } else {
+            SR.setPower(0);
+            SL.setPower(0);
+        }
+    }
+
+
+
+}
