9517bb60f0a3dedbb752913cad4b1eda68eed32c
pymbotics <jzhang2028@pymblelc.nsw.edu.au>
2026-05-12 15:50:56 +1000

    wrote robot code

--- src/main/java/org/firstinspires/ftc/teamcode/teleop_code.java
+++ src/main/java/org/firstinspires/ftc/teamcode/teleop_code.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp (name = "a11")
+@TeleOp (name = "a10")
 public class teleop_code extends OpMode {
 
     private DcMotor front_left, front_right, back_right, back_left;
