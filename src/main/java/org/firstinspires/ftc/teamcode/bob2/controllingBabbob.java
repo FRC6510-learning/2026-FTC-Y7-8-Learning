d8158d3c7037194b1562b1ac37127595f948462b
Asaphfirst <32269235+Asaphfirst@users.noreply.github.com>
2026-05-13 15:51:36 +1000

    6/5/2026 meowmeowmeowmeow

--- src/main/java/org/firstinspires/ftc/teamcode/bob2/controllingBabbob.java
+++ src/main/java/org/firstinspires/ftc/teamcode/bob2/controllingBabbob.java
@@ -4,11 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(group = "", name = "the best code that doesn't make any sense")
+@TeleOp(group = "", name = "the best code that doesn't make any sense 2")
 public class controllingBabbob extends OpMode {
     // Section 1 : defining variables
     DcMotor backbob, middlebob, shooterbobl, shooterbobr;
+
+    DcMotor FR, FL, BR, BL;
     CRServo frontbob;
 
 
@@ -22,6 +25,13 @@ public class controllingBabbob extends OpMode {
         frontbob = hardwareMap.get(CRServo.class, "top intake servo");
         shooterbobl = hardwareMap.get(DcMotor.class, "shooter left");
         shooterbobr = hardwareMap.get(DcMotor.class, "shooter right");
+        FR = hardwareMap.get(DcMotor.class, "front right");
+        FL = hardwareMap.get(DcMotor.class, "front left");
+        BR = hardwareMap.get(DcMotor.class, "back right");
+        BL = hardwareMap.get(DcMotor.class, "back left");
+
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
     }
@@ -29,31 +39,47 @@ public class controllingBabbob extends OpMode {
     public void loop(){
         // Section 3 : repeating bob
 
+        telemetry.addData("Servo Power", frontbob.getPower());
+
         if (gamepad1.a) {
-            backbob.setPower(-0.8);
-            middlebob.setPower(-0.8);
-            frontbob.setPower(-0.8);
+
+            backbob.setPower(-1);
+            middlebob.setPower(-1);
+            frontbob.setPower(1);
+
         } else if (gamepad1.b) {
-            backbob.setPower(0.8);
-            middlebob.setPower(0.8);
-            frontbob.setPower(0.8);
+
+            backbob.setPower(1);
+            middlebob.setPower(1);
+            frontbob.setPower(-1);
 
         }
         else {
+
             backbob.setPower(0);
             middlebob.setPower(0);
             frontbob.setPower(0);
+
         }
 
         if (gamepad1.y) {
-            shooterbobl.setPower(-0.8);
-            shooterbobr.setPower(0.8);
+
+            shooterbobl.setPower(-0.9);
+            shooterbobr.setPower(-0.9);
+
         } else if (gamepad1.x) {
+
             shooterbobl.setPower(-0.0);
             shooterbobr.setPower(0.0);
+
         }
 
 
+        FR.setPower(gamepad1.left_stick_y);
+        FL.setPower(gamepad1.left_stick_y);
+        BR.setPower(gamepad1.left_stick_y);
+        BL.setPower(gamepad1.left_stick_y);
 
+        telemetry.update();
     }
 }
