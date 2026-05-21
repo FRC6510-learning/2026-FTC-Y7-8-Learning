6840bd44d2a6417238f1a1a23e54c2002ad7cf6d
rteam <RTeam@pymblelc.nsw.edu.au>
2026-05-21 18:27:45 +1000

    robot_code

--- src/main/java/org/firstinspires/ftc/teamcode/bob2/controllingBabbob.java
+++ src/main/java/org/firstinspires/ftc/teamcode/bob2/controllingBabbob.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(group = "", name = "the best code that doesn't make any sense 2")
+@TeleOp(group = "", name = "the best code that doesn't make any sense 3")
 public class controllingBabbob extends OpMode {
     // Section 1 : defining variables
     DcMotor backbob, middlebob, shooterbobl, shooterbobr;
@@ -30,8 +30,10 @@ public class controllingBabbob extends OpMode {
         BR = hardwareMap.get(DcMotor.class, "back right");
         BL = hardwareMap.get(DcMotor.class, "back left");
 
-        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        FR.setDirection(DcMotorSimple.Direction.FORWARD);
+        BR.setDirection(DcMotorSimple.Direction.FORWARD);
         FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        BL.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
     }
@@ -42,43 +44,46 @@ public class controllingBabbob extends OpMode {
         telemetry.addData("Servo Power", frontbob.getPower());
 
         if (gamepad1.a) {
-
             backbob.setPower(-1);
             middlebob.setPower(-1);
             frontbob.setPower(1);
-
         } else if (gamepad1.b) {
-
             backbob.setPower(1);
             middlebob.setPower(1);
             frontbob.setPower(-1);
-
         }
         else {
-
             backbob.setPower(0);
             middlebob.setPower(0);
             frontbob.setPower(0);
-
         }
-
         if (gamepad1.y) {
-
             shooterbobl.setPower(-0.9);
             shooterbobr.setPower(-0.9);
-
         } else if (gamepad1.x) {
-
             shooterbobl.setPower(-0.0);
             shooterbobr.setPower(0.0);
-
         }
 
+        double left_y = gamepad1.left_stick_y;
+        double left_x = gamepad1.left_stick_x;
+        double right_x = gamepad1.right_stick_x;
 
-        FR.setPower(gamepad1.left_stick_y);
-        FL.setPower(gamepad1.left_stick_y);
-        BR.setPower(gamepad1.left_stick_y);
-        BL.setPower(gamepad1.left_stick_y);
+
+//        FR.setPower(left_y);
+//        FL.setPower(left_y);
+//        BR.setPower(left_y);
+//        BL.setPower(left_y);
+
+//        FR.setPower(left_x);
+//        FL.setPower(-left_x);
+//        BR.setPower(-left_x);
+//        BL.setPower(left_x);
+
+        FR.setPower(left_y + left_x + right_x);
+        FL.setPower(left_y - left_x - right_x);
+        BR.setPower(right_x + left_y - left_x);
+        BL.setPower(-right_x + left_y + left_x);
 
         telemetry.update();
     }
