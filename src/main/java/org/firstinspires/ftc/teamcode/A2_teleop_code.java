6b6d761dae1e108fd3a34373df71aed554b028cf
pymbotics <jzhang2028@pymblelc.nsw.edu.au>
2026-05-27 15:45:16 +1000

    27/5 auto update

--- src/main/java/org/firstinspires/ftc/teamcode/A2_teleop_code.java
+++ src/main/java/org/firstinspires/ftc/teamcode/A2_teleop_code.java
@@ -34,10 +34,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 /*
  * This file contains an example of an iterative (Non-Linear) "OpMode".
