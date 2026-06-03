76de753f1abe42360e5aef185b34c102f914d6cb
RTeam <rteam@pymblelc.nsw.edu.au>
2026-06-03 18:18:36 +1000

    hihihihi

--- src/main/java/org/firstinspires/ftc/teamcode/minicomp2026/comp_codemini_auto.java
+++ src/main/java/org/firstinspires/ftc/teamcode/minicomp2026/comp_codemini_auto.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous (name = "autocode_minicomp")
@@ -13,7 +14,7 @@ public class comp_codemini_auto extends LinearOpMode {
     DcMotor FR, FL, BR, BL;
     DcMotor Front_Intake_Right;
     DcMotor Middle_Intake_Left;
-    DcMotor Shooter_Right,Shooter_Left;
+    DcMotorEx Shooter_Right,Shooter_Left;
     CRServo Servo;
 
     @Override
@@ -23,8 +24,8 @@ public class comp_codemini_auto extends LinearOpMode {
         FL = hardwareMap.get(DcMotor.class, "FLMoonRover");
         BR = hardwareMap.get(DcMotor.class, "BRNeilArmstrong");
         BL = hardwareMap.get(DcMotor.class, "BLArtemis");
-        Shooter_Left = hardwareMap.get(DcMotor.class, "Chickedee(Left shooter)");
-        Shooter_Right = hardwareMap.get(DcMotor.class, "Chick(right shooter)");
+        Shooter_Left = hardwareMap.get(DcMotorEx.class, "Chickedee(Left shooter)");
+        Shooter_Right = hardwareMap.get(DcMotorEx.class, "Chick(right shooter)");
         Front_Intake_Right = hardwareMap.get(DcMotor.class, "Icecube(lower intake right)");
         Middle_Intake_Left = hardwareMap.get(DcMotor.class, "Penguin2 (2nd intake left)");
         Servo = hardwareMap.get(CRServo.class, "Arctic(shooter servo)");
@@ -66,13 +67,14 @@ public class comp_codemini_auto extends LinearOpMode {
         sleep(1000);
         forward(1000,0.6);
         sleep(1000);
-        shooter(0.90);
+        shooter(1900);
         sleep(1000);
         turn(-1009/2,0.6);
         sleep(1000);
         Servo.setPower(1);
         forward(1900,0.5);
         sleep(1700);
+        shooter(0);
         forward(-1500,0.5);
         turn(-980,0.5);
         forward(1500,0.5);
@@ -83,16 +85,16 @@ public class comp_codemini_auto extends LinearOpMode {
 
     }
 
-    public  void shooter(double power){
-        Shooter_Left.setPower(power);
-        Shooter_Right.setPower(power);
+    public  void shooter(int power){
+        Shooter_Left.setVelocity(power);
+        Shooter_Right.setVelocity(power);
     }
 
     public void forward(int distance, double power) {
-        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        FL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
         FR.setTargetPosition(distance);
         FR.setPower(power);
@@ -106,10 +108,10 @@ public class comp_codemini_auto extends LinearOpMode {
         BL.setTargetPosition(distance);
         BL.setPower(power);
 
-        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        FL.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        BR.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
         while (FR.isBusy() || FL.isBusy() || BR.isBusy() || BL.isBusy()) {
 
@@ -119,10 +121,10 @@ public class comp_codemini_auto extends LinearOpMode {
 
     }
     public void something(int FR_, int FL_, int BR_, int BL_, double power) {
-        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        FL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
         FR.setTargetPosition(FR_);
         FR.setPower(power);
