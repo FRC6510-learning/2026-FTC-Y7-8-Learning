832a1e589755c6a1128abb4009674ae5fbda219e
SavenyaM <smaganaarachchi2028@pymblelc.nsw.edu.au>
2026-06-03 18:16:39 +1000

    Added auto

---
+++ src/main/java/org/firstinspires/ftc/teamcode/samples/Auto_Robot_Code.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous(name="a4_auto_code", group="2")
+
+public class Auto_Robot_Code extends LinearOpMode
+{
+    //Setup
+
+    private DcMotor FR, FL, BR, BL, I, PF, SL, SR;
+    private CRServo F;
+
+    public void move(int distance, double speed){
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FR.setTargetPosition(distance);
+        FL.setTargetPosition(distance);
+        BR.setTargetPosition(distance);
+        BL.setTargetPosition(distance);
+
+        FR.setPower(speed);
+        FL.setPower(speed);
+        BR.setPower(speed);
+        BL.setPower(speed);
+
+
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while(FR.isBusy() || FL.isBusy() || BR.isBusy() || BL.isBusy()) {}
+
+
+    }
+
+    @Override
+    public void runOpMode(){
+        //Auto Code
+        FR = hardwareMap.get(DcMotor.class,"FR");
+        FL = hardwareMap.get(DcMotor.class,"FL");
+        BR = hardwareMap.get(DcMotor.class,"BR");
+        BL = hardwareMap.get(DcMotor.class,"BL");
+        I = hardwareMap.get(DcMotor.class,"I");
+        PF = hardwareMap.get(DcMotor.class,"PF");
+        SL = hardwareMap.get(DcMotor.class,"SL");
+        SR = hardwareMap.get(DcMotor.class,"SR");
+
+        F = hardwareMap.get(CRServo.class,"F");
+
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BR.setDirection(DcMotorSimple.Direction.FORWARD);
+        FL.setDirection(DcMotorSimple.Direction.FORWARD);
+        BL.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //Auto program
+        move(1000,0.5);
+
+        move(1500, 0.7);
+    }
+}
