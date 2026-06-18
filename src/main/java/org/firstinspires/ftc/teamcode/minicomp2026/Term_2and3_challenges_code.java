129bb0147721e5636e99b4b6f05e45cebdd5f17c
Robotics-Team-6510 <RTeam@pymblelc.nsw.edu.au>
2026-06-18 18:16:35 +1000

    MENTOR PUSH 18/6

---
+++ src/main/java/org/firstinspires/ftc/teamcode/minicomp2026/Term_2and3_challenges_code.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.minicomp2026;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "")
+public class Term_2and3_challenges_code extends OpMode {
+
+
+
+
+
+
+    @Override
+    public void init(){
+
+        FR = hardwareMap.get(DcMotor.class,"FRApollo12");
+        FL = hardwareMap.get(DcMotor.class,"FLMoonRover");
+        BR = hardwareMap.get(DcMotor.class,"BRNeilArmstrong");
+        BL = hardwareMap.get(DcMotor.class,"BLArtemis");
+        ShooterLeft = hardwareMap.get(DcMotor.class,)
+
+
+
+
+
+    }
+    @Override
+    public void loop(){
+
+
+
+
+    }
+}
