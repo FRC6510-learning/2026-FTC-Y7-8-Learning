fca524b780b7c102c07c26f5d17b58cecde44e98
3mm4-0 <eouyang2029@pymblelc.nsw.edu.au>
2026-07-29 17:19:24 +1000

    Field centric code

--- src/main/java/org/firstinspires/ftc/teamcode/NEW_Baked_Not_Fried.java
+++ src/main/java/org/firstinspires/ftc/teamcode/NEW_Baked_Not_Fried.java
@@ -1,15 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "baked_not_fried")
 public class NEW_Baked_Not_Fried extends OpMode {
     // 1 - declare variables
 
     DcMotor RF, RB, LF, LB;
+
+    IMU imu;
 
     @Override
     public void init(){
@@ -20,6 +26,13 @@ public class NEW_Baked_Not_Fried extends OpMode {
         LF = hardwareMap.get(DcMotor.class, "lf");
         LB = hardwareMap.get(DcMotor.class, "lb");
 
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+// Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
 
         RF.setDirection(DcMotorSimple.Direction.FORWARD);
         RB.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -34,13 +47,18 @@ public class NEW_Baked_Not_Fried extends OpMode {
         double move = gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
         double strafe = gamepad1.left_stick_x;
+        double direction = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double RotStrafe = strafe * Math.cos(-direction) - move * Math.sin(-direction);
+        double RotMove = strafe * Math.sin(-direction) + move * Math.cos(-direction);
 
         RF.setPower(move + turn + strafe);
         RB.setPower(move + turn -  strafe);
         LF.setPower(move - turn - strafe);
         LB.setPower(move - turn + strafe);
 
-
+        telemetry.addData("imu", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+        telemetry.update();
 
     }
 
