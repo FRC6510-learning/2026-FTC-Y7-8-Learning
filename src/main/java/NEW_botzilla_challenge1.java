5bfbf63df2e3ca6815dd3d8a0fe8cad28d3c3ac7
SavenyaM <smaganaarachchi2028@pymblelc.nsw.edu.au>
2026-07-30 18:14:37 +1000

    i make understanding

--- src/main/java/NEW_botzilla_challenge1.java
+++ src/main/java/NEW_botzilla_challenge1.java
@@ -1,7 +1,13 @@
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp(name = "i make understanding :)")
 public class NEW_botzilla_challenge1 extends OpMode {
     // 1 - varibale definition
 
@@ -12,6 +18,8 @@ public class NEW_botzilla_challenge1 extends OpMode {
     DcMotor BR;
     DcMotor BL;
 
+    IMU imu;
+
 
 
     @Override
@@ -21,6 +29,13 @@ public class NEW_botzilla_challenge1 extends OpMode {
         FL = hardwareMap.get(DcMotor.class, "front left");
         BL = hardwareMap.get(DcMotor.class, "back left");
         BR = hardwareMap.get(DcMotor.class, "back right");
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.DOWN));
+        imu.initialize(parameters);
 
         FR.setDirection(DcMotorSimple.Direction.FORWARD);
         BR.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -44,6 +59,9 @@ public class NEW_botzilla_challenge1 extends OpMode {
         FL.setPower(forward + turning + sideways);
         BR.setPower(forward - turning + sideways);
         BL.setPower(forward + turning - sideways);
+
+        telemetry.addData("caption", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+        telemetry.update();
 
       
     }
