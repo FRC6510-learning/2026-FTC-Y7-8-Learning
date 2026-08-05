ab9af7a5c22cc0a95eab008483fc597fcff5e205
pymbotics <jzhang2028@pymblelc.nsw.edu.au>
2026-08-05 16:39:55 +1000

    23/07/2026 new teleop

--- src/main/java/NEW_challenge1.java
+++ src/main/java/NEW_challenge1.java
@@ -2,22 +2,32 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 
 @TeleOp(name = "DogSausageTeleop")
 public class NEW_challenge1 extends OpMode {
     // section 1 - declaring variables - making variables
     private DcMotor FrontRight, FrontLeft, BackLeft, BackRight;
+    private IMU imu;
 
     @Override
     public void init(){
         // linking to config
-        FrontRight = hardwareMap.get(DcMotor .class, "FRW");
+        FrontRight = hardwareMap.get(DcMotor.class, "FRW");
         FrontLeft = hardwareMap.get(DcMotor.class, "FLW");
         BackLeft = hardwareMap.get(DcMotor.class, "BLW");
         BackRight = hardwareMap.get(DcMotor.class, "BRW");
 
         FrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         BackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        imu = hardwareMap.get(IMU.class, "IMU");
+        RevHubOrientationOnRobot Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);
+        IMU.Parameters Para = new IMU.Parameters(Orientation);
+        imu.initialize(Para);
 
 
 
@@ -37,11 +47,21 @@ public class NEW_challenge1 extends OpMode {
             speed = 0.3;
         }
 
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
 
-        FrontLeft.setPower((forward + turn + strafe) * speed);
-        FrontRight.setPower((forward - turn - strafe) * speed);
-        BackLeft.setPower((forward + turn - strafe) * speed);
-        BackRight.setPower((forward - turn + strafe) * speed);
+        double angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        double x = strafe * Math.cos(-angle) - forward * Math.sin(-angle);
+        double y = strafe * Math.sin(-angle) + forward * Math.cos(-angle);
+        x = x * 1.1;
+
+        double denominator = Math.max(Math.abs(x) + Math.abs(y) + Math.abs(turn), 1);
+
+        FrontLeft.setPower((y + turn + x) / denominator * speed);
+        FrontRight.setPower((y - turn - x) / denominator * speed);
+        BackLeft.setPower((y + turn - x) / denominator * speed);
+        BackRight.setPower((y - turn + x) / denominator * speed);
 
 
 
